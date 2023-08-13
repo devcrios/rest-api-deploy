09dc9fec67a3b0b3caa8cac32ad09392306c871e
crios_dev <dev.crios@gmail.com>
2023-08-12 19:28:36 -0500

    Depploy API to FL0

---
+++ app.js
@@ -0,0 +1,147 @@
+const express = require("express");
+const crypto = require("node:crypto");
+const cors = require('cors')
+
+const movies = require("./movies.json");
+const { validateMovie, validatePartialMovie } = require("./schemas/movies");
+
+
+const app = express();
+app.use(express.json());
+app.use(cors({
+  origin: (origin, callback) => {
+    const ACCEPTED_ORIGINS = [
+      'http://localhost:8080',
+      'http://localhost:1234',
+      'https://movies.com'
+    ]
+
+    if (ACCEPTED_ORIGINS.includes(origin)) {
+      return callback(null, true)
+    }
+
+    if (!origin) {
+      return callback(null, true)
+    }
+
+    return callback(new Error('Not allowed by CORS'))
+  }
+}))
+
+app.disable("x-powered-by");
+
+//metodos normales: GET-HEAD-POST
+//metodos complejos:PUT/PATCH/DELETE REquiere una peticion especial llamada OPTIONS
+
+//CORS PRE-Flight
+
+// const ACCEPTED_ORIGINS = [
+//   'http://localhost:8080',
+//   'http://localhost:3000',
+//   'http://localhost:1234',
+//   'http://movies.com'
+// ]
+
+//todos los recursos que sean MOVIES se identifican con /movies
+app.get("/movies", (req, res) => {
+  // const origin = req.header('origin') //el navegador no envia el ORIGIN cuando la 
+  // //peticion de hace del mismo servidor http://localhost:1234 --->> http://localhost:1234
+  // if (ACCEPTED_ORIGINS.includes(origin) || !origin) {
+  //   res.header('Access-Control-Allow-Origin', origin)
+  // }
+
+  const { genre } = req.query;
+  if (genre) {
+    const filteredMOvies = movies.filter((movie) =>
+      movie.genre.some((g) => g.toLowerCase() === genre.toLocaleLowerCase())
+    );
+    return res.json(filteredMOvies);
+  }
+  res.json(movies);
+});
+
+app.get("/movies/:id", (req, res) => {
+  //path-to-regexp
+  const { id } = req.params;
+  const movie = movies.find((movie) => movie.id === id);
+  if (movie) return res.json(movie);
+  res.status(404).json({ message: "Movie not found" });
+});
+
+app.post("/movies", (req, res) => {
+  const result = validateMovie(req.body);
+
+  if (result.error) {
+    return res.status(400).json({ error: JSON.parse(result.error.message) });
+  }
+
+  const newMovie = {
+    id: crypto.randomUUID(),
+    ...result.data,
+  };
+  // Esto no seria REST,porque estamos guardando
+  // el estdo de la aplicación en memoria
+
+  movies.push(newMovie);
+
+  res.status(201).json(newMovie); //es importante enviar el recurso creado para actualizar la chaché del cliente
+});
+
+app.delete('/movies/:id', (req, res) => {
+  // const origin = req.header('origin')
+
+  // if (ACCEPTED_ORIGINS.includes(origin) || !origin) {
+  //   res.header('Access-Control-Allow-Origin', origin)
+  // }
+
+  const { id } = req.params;
+  const movieIndex = movies.findIndex(movie => movie.id === id);
+
+  if (movieIndex === -1) {
+    return res.status(404).json({ message: "Movie not found" });
+  }
+
+  movies.splice(movieIndex, 1)
+
+  return res.json({ message: 'Movie deleted' })
+})
+
+app.patch("/movies/:id", (req, res) => {
+  const result = validatePartialMovie(req.body);
+
+  if (!result.success) {
+    return res.status(400).json({ error: JSON.parse(result.error.message) });
+  }
+
+  const { id } = req.params;
+  const movieIndex = movies.findIndex(movie => movie.id === id);
+
+  if (movieIndex === -1) {
+    return res.status(404).json({ message: "Movie not found" });
+  }
+
+  const updateMovie = {
+    ...movies[movieIndex],
+    ...result.data
+  };
+
+  movies[movieIndex] = updateMovie;
+
+  return res.json(updateMovie);
+});
+
+// app.options('/movies/:id', (req, res) => {
+//   const origin = req.header('origin')
+
+//   if (ACCEPTED_ORIGINS.includes(origin) || !origin) {
+//     res.header('Access-Control-Allow-Origin', origin)
+//     res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE')
+//   }
+//   res.send(200)
+// })
+
+const PORT = process.env.PORT ?? 1234;
+
+app.listen(PORT, () => {
+  console.log(`server listening on port http://localhost:${PORT}`);
+});
